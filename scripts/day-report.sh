0b7cdd58a98a3c2e7ab292b545658d7098e44f54
Maxim Gabidullin <max@gabidullin.site>
2022-12-03 02:41:22 +0300

    day-report-sh

--- scripts/day-report.sh
+++ scripts/day-report.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www/v2.api.aftaa.ru/
-/usr/bin/docker compose exec php symfony console app:day-report
-cd -
+#/usr/bin/docker compose exec php symfony console app:day-report
+/home/max/.docker/cli-plugins/docker-compose exec php symfony console app:day-report
